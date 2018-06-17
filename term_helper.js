494d3512ec7a3bf6efbcf09c4dc0d6933d98c188
Jens Kerrinnes <jens.kerrinnes@gmail.com>
2018-06-17 20:23:46 +0200

    Yeah

---
+++ term_helper.js
@@ -0,0 +1,33 @@
+class helper {
+	static bytes_to_signed(lsb, msb){
+		var sign = msb & (1 << 7);
+		var x = (((msb & 0xFF) << 8) | (lsb & 0xFF));
+		if (sign) {
+			return  (0xFFFF0000 | x);  // fill in most significant bits with 1's
+		}else{
+			return  x;
+		}
+	}
+	
+	static convertArrayBufferToString(buf){
+		var bufView = new Uint8Array(buf);
+		var encodedString = String.fromCharCode.apply(null, bufView);
+		var str = decodeURIComponent(encodedString);
+		return str;
+	}
+
+	static convertStringToArrayBuffer(str) {
+		var buf=new ArrayBuffer(str.length);
+		var bufView=new Uint8Array(buf);
+		for (var i=0; i<str.length; i++) {
+			bufView[i]=str.charCodeAt(i);
+		}
+		return buf;
+	}
+	
+	static delay(ms) {
+		ms += new Date().getTime();
+		while (new Date() < ms){}
+	}
+	
+}
